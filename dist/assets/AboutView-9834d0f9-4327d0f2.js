c91a524b50dff9c0ba6d65ba9a514fede592940f
matownsend4 <matownsend4@crimson.ua.edu>
2023-11-11 09:20:52 -0600

    changed title

--- dist/assets/AboutView-9834d0f9-4327d0f2.js
+++
@@ -1,1 +0,0 @@
-import{B as a,e,O as s,j as t}from"./index-a4ef1171.js";const n={},o={class:"about"},r=a("h1",null,"This is an about page",-1),u=[r];function i(c,l){return s(),t("div",o,u)}const f=e(n,[["render",i]]);export{f as default};
